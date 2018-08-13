8068036e3fb81d68457e2b262498ed428bdb3e49
monicarajendran <monicarajendran96@gmail.com>
2018-08-13 16:05:29 +0530

    Changes for test cases

---
+++ Example/Tests/ConstantsTest.swift
@@ -0,0 +1,28 @@
+//
+//  ConstantsTest.swift
+//  FullAuthIOSClient_Tests
+//
+//  Created by Monica on 13/08/18.
+//  Copyright © 2018 CocoaPods. All rights reserved.
+//
+
+import XCTest
+import FullAuthIOSClient
+
+class ConstantsTest: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+        
+    }
+
+    func testPerformanceExample() {
+        self.measure {
+        }
+    }
+    
+}
